50fc4dbd706b040a2f46361271f2dc9db8866f4c
Andrii Pereverziev <a.pereverziev@enamine.net>
2022-09-08 14:17:23 -0700

    Logo

--- public/index.php
+++ public/index.php
@@ -23,7 +23,7 @@
 <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="mb-auto">
     <div>
-      <h3 class="float-md-start mb-0">Eight Green Jumpers FRAMEWORK</h3>
+      <img src="/images/logo.svg" style="width: 300px;" alt="logo">
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <a class="nav-link fw-bold py-1 px-0 active" aria-current="page" href="/">Home</a>
       </nav>
